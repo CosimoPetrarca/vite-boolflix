272e003d73851e431945f2b0a2b75ef462b709a3
P4anix <pancok_1@yahoo.it>
2023-03-30 07:52:39 +0200

    install sass and fix structure Milestone 1

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -16,28 +16,46 @@ export default {
 </script>
 
 <template>
-    <div>
-        <h1>BoolFix</h1>
-        <!-- input di ricerca -->
-        <form @submit.prevent="$emit('searchName')">
-            <input type="text" placeholder="Cerca un film" v-model="store.searchText">
-            <button type="submit">Ricerca</button>
-        </form>
-        <!-- /input di ricerca -->
+    <div class="container">
+        <div>
+            <h1>BoolFix</h1>
+            <!-- input di ricerca -->
+            <form @submit.prevent="$emit('searchName')">
+                <input type="text" placeholder="Cerca un film" v-model="store.searchText">
+                <button type="submit">Ricerca</button>
+            </form>
+            <!-- /input di ricerca -->
+        </div>
 
         <!-- lista characters movies -->
-        <ul>
-            <li v-for="movie in store.movies">
-                <ul>
-                    <li><strong>{{ movie.title }}</strong></li>
-                    <li>{{ movie.original_title }}</li>
-                    <li>{{ movie.original_language }}</li>
-                    <li>{{ movie.vote_average }}</li>
-                </ul>
-            </li>
+
+        <ul v-for="movie in store.movies">
+            <li><strong>{{ movie.title }}</strong></li>
+            <li>{{ movie.original_title }}</li>
+            <li>{{ movie.original_language }}</li>
+            <li>{{ movie.vote_average }}</li>
         </ul>
+
         <!-- /lista characters movies -->
     </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.container {
+    max-width: 1200px;
+    margin: auto;
+
+    div {
+
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+
+    ul{
+        padding: 15px 0;
+    }
+
+
+}
+</style>
